0809b56775852077f0a096a248689eeee5cd663d
ErReich <nikolya.severus@mail.ru>
2021-02-18 18:05:38 +0300

    Bootstraaaaaaaap!

--- src/main/java/com/springboot/crud_security/controller/AdminController.java
+++ src/main/java/com/springboot/crud_security/controller/AdminController.java
@@ -5,11 +5,14 @@ import com.springboot.crud_security.entity.User;
 import com.springboot.crud_security.service.RoleService;
 import com.springboot.crud_security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -20,31 +23,39 @@ public class AdminController {
 
     private final UserService userService;
     private final RoleService roleService;
-    private final BCryptPasswordEncoder passwordEncoder;
-    private String patchedPassword;
+    private final BCryptPasswordEncoder passwordEncoder;//--
+    private String patchedPassword;//--
+    private final UserDetailsService userDetailsService;
 
     @Autowired
     public AdminController(UserService userService
-            , RoleService roleService, BCryptPasswordEncoder passwordEncoder) {
+            , RoleService roleService, BCryptPasswordEncoder passwordEncoder
+            , @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService) {
         this.userService = userService;
         this.roleService = roleService;
         this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
     }
 
     @GetMapping
-    public String getAllUsers(Model model) {
+    public String getAllUsers(Model model, Principal principal) {
+
+        User principalUser = (User) userDetailsService.loadUserByUsername(principal.getName());
+
         model.addAttribute("users", userService.getAllUsers());
-
-        return "all_users";
-    }
-
-    @GetMapping("/add-new-user")
-    public String addUser(Model model) {
+        model.addAttribute("principalUser", principalUser);
         model.addAttribute("new_user", new User());
-//        model.addAttribute("allRoles", roleService.getAllRoles());
 
-        return "user_create";
+        return "admin_page";
     }
+
+//    @GetMapping("/add-new-user")
+//    public String addUser(Model model) {
+//        model.addAttribute("new_user", new User());
+////        model.addAttribute("allRoles", roleService.getAllRoles());
+//
+//        return "user_create";
+//    }
 
     @PostMapping
     public String saveUser(@ModelAttribute("new_user") User user
@@ -58,41 +69,65 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/{id}/update")
-    public String updateInfo(@PathVariable(name = "id") Integer id, Model model) {
-        User patchedUser = userService.getUserById(id);
-        patchedPassword = patchedUser.getPassword();
+//    @GetMapping("/{id}/update")
+//    public String updateInfo(@PathVariable(name = "id") Integer id, Model model) {
+//        User patchedUser = userService.getUserById(id);
+//        patchedPassword = patchedUser.getPassword();
+//
+//        model.addAttribute("patchedUser", patchedUser);
+////        model.addAttribute("allRoles", roleService.getAllRoles());
+//
+//        return "user_update";
+//    }
 
-        model.addAttribute("patchedUser", patchedUser);
-//        model.addAttribute("allRoles", roleService.getAllRoles());
-
-        return "user_update";
-    }
+//    @PatchMapping
+//    public String updateUser(@ModelAttribute("patchedUser") User user
+//                            , @RequestParam("patched_roles") String[] roles) {
+//
+//        user.setRoles(getSetFromArray(roles));
+//
+////        if (!patchedPassword.equals(user.getPassword())) {
+////            user.setPassword(passwordEncoder.encode(user.getPassword()));
+////        }
+//
+//        userService.saveOrUpdateUser(user);
+//
+//        return "redirect:/admin";
+//    }
 
     @PatchMapping
-    public String updateUser(@ModelAttribute("patchedUser") User user
-                            , @RequestParam("patched_roles") String[] roles) {
+    public String updateUser(@RequestParam("edit_id") int id
+            , @RequestParam("edit_name") String name
+            , @RequestParam("edit_surname") String surname
+            , @RequestParam("edit_age") byte age
+            , @RequestParam("edit_email") String email
+            , @RequestParam("edit_password") String password
+            , @RequestParam("edit_roles") String[] roles) {
 
+        User user = new User();
+
+        user.setId(id);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setAge(age);
+        user.setEmail(email);
+        user.setPassword(password);
         user.setRoles(getSetFromArray(roles));
-
-        if (!patchedPassword.equals(user.getPassword())) {
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-        }
 
         userService.saveOrUpdateUser(user);
 
         return "redirect:/admin";
     }
 
-    @GetMapping("/{id}/delete")
-    public String deleteInfo(@PathVariable(name = "id") Integer id, Model model) {
-        model.addAttribute("deletedUser", userService.getUserById(id));
-        return "user_delete";
-    }
+//    @GetMapping("/{id}/delete")
+//    public String deleteInfo(@PathVariable(name = "id") Integer id, Model model) {
+//        model.addAttribute("deletedUser", userService.getUserById(id));
+//        return "user_delete";
+//    }
 
     @DeleteMapping
-    public String deleteUser(@ModelAttribute("deletedUser") User user) {
-        userService.deleteUser(user);
+    public String deleteUser(@RequestParam("delete_id") int id) {
+        userService.deleteUser(userService.getUserById(id));
         return "redirect:/admin";
     }
 
